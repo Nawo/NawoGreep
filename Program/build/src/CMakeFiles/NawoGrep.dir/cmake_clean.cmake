7dd22e4bfc0ff373ae861ce98a6ba82371ddaf3d
Roman Nawrot <romug1337@gmail.com>
2023-02-26 10:04:45 +0100

    fix

---
+++ Program/build/src/CMakeFiles/NawoGrep.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NawoGrep.dir/FindedFiles.cpp.obj"
+  "CMakeFiles/NawoGrep.dir/FindedFiles.cpp.obj.d"
+  "CMakeFiles/NawoGrep.dir/Grep.cpp.obj"
+  "CMakeFiles/NawoGrep.dir/Grep.cpp.obj.d"
+  "CMakeFiles/NawoGrep.dir/main.cpp.obj"
+  "CMakeFiles/NawoGrep.dir/main.cpp.obj.d"
+  "NawoGrep.exe"
+  "NawoGrep.exe.manifest"
+  "NawoGrep.pdb"
+  "libNawoGrep.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NawoGrep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Program/build/src/CMakeFiles/NawoGrep.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NawoGrep.
+# This may be replaced when dependencies are built.
